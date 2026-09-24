928c1c748e1548eb97590541e6ace7403913b76a
Iannn-dx <tevesiankhristopher@gmail.com>
2026-09-24 15:21:38 +0800

    lab activity: contacts

---
+++ contacts.js
@@ -0,0 +1,29 @@
+function contacts() {
+    var name = "Maxwell Wright";
+    var phone = "(0191) 719 6495";
+    var email = "Curabitur.egestas.nunc@nonummyac.co.uk";
+    var name1 = name;
+    var phone1 = phone;
+    var email1 = email;
+    {
+        var name = "Raja Villareal";
+        var phone = "(0866) 398 2895";
+        var email = "posuere.vulputate@sed.com";
+        var name2 = name;
+        var phone2 = phone;
+        var email2 = email;
+    }
+    {
+        var name = "Helen Richards";
+        var phone = "0800 1111";
+        var email = "libero@convallis.edu";
+        var name3 = name;
+        var phone3 = phone;
+        var email3 = email;
+    }
+    console.log("Name: ", name1 + ", " + name2 + ", " + name3);
+    console.log("Phone: ", phone1 + ", " + phone2 + ", " + phone3);
+    console.log("Email: ", email1 + ", " + email2 + ", " + email3);
+}
+
+contacts();
--- intro.js
+++ intro.js
@@ -1,1 +1,81 @@
+"use strict";
+
 console.log("123");
+
+// sample
+let counter = 100;
+console.log(counter);
+counter = 120;
+console.log(counter);
+
+counter += 100;
+console.log(counter);
+
+// with scope
+let count;
+console.log(counter); {
+    count = 2;
+    console.log(count);
+}
+count += 3;
+console.log(count);
+
+// indention and local x global
+let height = 157; //global
+{
+    let weight = 80; //local
+    {
+        let info = "tall";
+        console.log(height);
+    }
+    // console.log(info);   local
+    console.log(height);
+}
+// console.log(weight); local
+// console.log(height); global
+// console.log(info); local
+
+// functions
+function testFunction() {
+    console.log("123");
+    console.log("456");
+}
+
+// testFunction();
+
+var globalGreeting = "good";
+
+function testingFunction() {
+    var localGreeting = "night";
+    console.log("Function: ");
+    console.log(globalGreeting);
+    console.log(localGreeting);
+}
+
+testingFunction();
+console.log(globalGreeting);
+// console.log(localGreeting); local undefined.
+
+function flowers() {
+    const price = 8;
+    let quantity = 70;
+    let newQuantity = quantity - 20;
+    let value = price * newQuantity;
+    var rose = value;
+    {
+        const price = 10;
+        let quantity = 50;
+        let newQuantity = quantity - 30;
+        let value = price * quantity;
+        var lily = value;
+        {
+            const price = 2;
+            let quantity = 120;
+            let value = price * quantity;
+            var tulip = value;
+        }
+    }
+    return console.log("Total: ", rose + lily + tulip);
+}
+
+flowers();
